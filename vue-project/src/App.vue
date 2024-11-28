8afe4d76d7c25ee32b9c7e0dd2e07db67dd6ce4b
Roberto <robertodelgado842@gmail.com>
2024-11-28 14:51:59 -0600

    tailwinf-instalado

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+//import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h1 className="text-6xl font-bold underline">Hello world!</h1>
+  <h1 className="text-4xl font-bold underline">Hello world!</h1>
 </template>
 
 <style scoped>
